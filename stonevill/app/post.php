bb7c4c4535f6fc99e8c75965f084c7d1938a466c
lavrenyukmaksim <41396563+lavrenyukmaksim@users.noreply.github.com>
2018-07-19 03:08:05 +0300

    Add files via upload

---
+++ stonevill/app/post.php
@@ -0,0 +1,103 @@
+ 
+<? 
+// ----------------------------конфигурация-------------------------- // 
+ 
+$adminemail="maksim.lavrenyuk@gmail.com";  // e-mail админа 
+ 
+ 
+$date=date("d.m.y"); // число.месяц.год 
+ 
+$time=date("H:i"); // часы:минуты:секунды 
+ 
+$backurl="http://xn--80aeahmkvnhb6amo4k.xn--p1ai/katrina.html";  // На какую страничку переходит после отправки письма 
+ 
+//---------------------------------------------------------------------- // 
+ 
+  
+ 
+// Принимаем данные с формы 
+ 
+$name=$_POST['name']; 
+ 
+$email=$_POST['email']; 
+
+$theme=$_POST['theme'];
+
+$msg=$_POST['message']; 
+ 
+  
+ 
+// Проверяем валидность e-mail 
+ 
+if (!preg_match("|^([a-z0-9_\.\-]{1,20})@([a-z0-9\.\-]{1,20})\.([a-z]{2,4})|is", 
+strtolower($email))) 
+ 
+ { 
+ 
+  echo 
+"<center>Вернитесь <a 
+href='javascript:history.back(1)'><B>назад</B></a>. Вы 
+указали неверные данные!"; 
+ 
+  } 
+ 
+ else 
+ 
+ { 
+ 
+ 
+$msg=" 
+ 
+ 
+Имя: $name
+<br>
+ 
+E-mail: $email
+<br>
+
+Телефон: $theme
+<br>
+ 
+Сообщение: $msg
+<br>
+ 
+ 
+"; 
+ 
+  
+ 
+ // Отправляем письмо админу  
+ 
+mail("$adminemail", "$date $time Сообщение 
+от $name", "$theme", "$msg"); 
+ 
+  
+ 
+// Сохраняем в базу данных 
+ 
+$f = fopen("katrina.txt", "a+"); 
+ 
+fwrite($f," \n $date $time Сообщение от $name"); 
+ 
+fwrite($f,"\n $msg "); 
+ 
+fwrite($f,"\n ---------------"); 
+ 
+fclose($f); 
+ 
+  
+ 
+// Выводим сообщение пользователю 
+ 
+print "<script language='Javascript'><!-- 
+function reload() {location = \"$backurl\"}; setTimeout('reload()', 6000); 
+//--></script> 
+ 
+$msg 
+ 
+<p>Сообщение отправлено! Подождите, сейчас вы будете перенаправлены на главную страницу...</p>";  
+exit; 
+ 
+ } 
+ 
+?>
